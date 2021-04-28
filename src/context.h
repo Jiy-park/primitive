516c81ce76537e8f5325cb85fe700dad0a27c725
park_Ji_Yong <jiyong3954@gmail.com>
2021-04-28 19:14:16 +0900

    중간고사 대체 과제 끝

--- src/context.h
+++ src/context.h
@@ -30,9 +30,10 @@ private:
     BufferUPtr m_indexBuffer;
     TextureUPtr m_texture;
     TextureUPtr m_texture2;
-    
+    TextureUPtr m_texture3;
+
     // clear color
-    glm::vec4 m_clearColor{glm::vec4(0.1f, 0.2f, 0.3f, 0.0f)};
+    glm::vec4 m_clearColor{glm::vec4(0.5f,1.0f,0.8f,0.5f)};
 
     // camera parameter
 
@@ -46,26 +47,34 @@ private:
 
     int m_width{WINDOW_WIDTH};
     int m_height{WINDOW_HEIGHT};
-    uint32_t m_indexCount {0};
 
+
+    uint32_t m_indexCount {0};      //indices_count
+    uint32_t m_vertices_count;      //vertices_count
+    uint32_t m_triangle_count {0};  //triangle_count
 
 
     const float PI=3.141592f;
+    bool for_call_Create_func_once=false; 
 
-    int donut_segment {40};        //  donut_element
-    int circle_segment {40};       //  
-    float donut_radius {5.0f};     //
-    float circle_radius {2.0f};    //
+    int donut_segment {8};        //  donut_elements
+    int circle_segment {8};       //  
+    float donut_radius {2.0f};     //  (0,0,0) ~ circle_center
+    float circle_radius {1.0f};    //
 
-    float cylinder_top_radius {5.0f};        //  cylinder_element
-    float cylinder_bottom_radius {5.0f};     //
-    float cylinder_height {4.0f};            //
+    float cylinder_top_radius {1.0f};        //  cylinder_elements
+    float cylinder_bottom_radius {1.0f};     //
+    float cylinder_height {1.0f};            //
     int segment {10};                        //
 
-    float user_radius {5.0f};        //  sphere_element
+    float user_radius {1.0f};        //  sphere_elements
     int width_segment {10};          //
     int height_segment {10};         //
-    
+
+    float time_for_autorotation=0.0f;
+    glm::vec3 rotation = glm::vec3(0.0f, 0.0f, 0.0f);
+    glm::vec3 rotate_speed=glm::vec3(0.0f,0.0f,0.0f);
+    glm::vec3 scale=glm::vec3(1.0f,1.0f,1.0f);
 };
 
 #endif // __CONTEXT_H__
